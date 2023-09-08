e2dc1e390ab77957b0b90bb262db9816b64fa21f
Nikolay Rogozin <norogozin@gmail.com>
2023-09-08 21:45:05 +0400

    basic CRUD functionality added

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,12 @@
 // @ts-check
 
 import i18next from 'i18next';
-import bcrypt from 'bcrypt';
 
 export default (app) => {
   app
     .get('/users', { name: 'users' }, async (req, reply) => {
       const users = await app.objection.models.user.query();
+      console.log('users: ', users);
       reply.render('users/index', { users });
       return reply;
     })
@@ -18,44 +18,75 @@ export default (app) => {
       const user = new app.objection.models.user();
       user.$set(req.body.data);
       try {
-        const validUser = await app.objection.models.user.fromJson(req.body.data);
+        const validUser = app.objection.models.user.fromJson(req.body.data);
         await app.objection.models.user.query().insert(validUser);
-        req.flash('info', i18next.t('flash.users.create.success'));
+        req.flash('info', i18next.t('flash.user.create.success'));
         reply.redirect(app.reverse('root'));
-      } catch ({ data }) {
+        await req.logIn(user);
+      } catch (error) {
+        console.log('could not create new user');
         req.flash('error', i18next.t('flash.users.create.error'));
-        reply.render('users/new', { user, errors: data });
+        reply.render('users/new', { user, error });
       }
-
       return reply;
     })
-    .post('/users/register', async (req, reply) => {
-      console.log('req: ', req);
-      const userModel = app.objection.models.user;
-      const {
-        firstName, lastName, email, password,
-      } = req.body;
-
+    .get('/users/:id/edit', { preValidation: app.authenticate }, async (req, reply) => {
+      const userId = req.params.id;
+      if (req.user.id !== Number(userId)) {
+        reply.redirect(app.reverse('users'));
+        return reply;
+      }
+      let user = null;
       try {
-        const existingUser = await userModel.query().where('email', email).first();
-        if (existingUser) {
-          throw new Error('User with this email already exists.');
+        user = await app.objection.models.user.query()
+          .findById(userId);
+        if (!user) {
+          return reply.status(404).send('User not found');
         }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        await userModel.query().insert({
-          firstName,
-          lastName,
-          email,
-          password: hashedPassword,
-        });
-
-        req.flash('info', i18next.t('flash.users.create.success'));
+      } catch (error) {
+        console.error(error);
+        reply.status(500).send('Internal Server Error');
+      }
+      reply.render('users/new', { user, editing: true });
+      return reply;
+    })
+    .patch('/users/:id', async (req, reply) => {
+      const user = new app.objection.models.user();
+      user.$set(req.body.data);
+      try {
+        const validUser = app.objection.models.user.fromJson(req.body.data);
+        await app.objection.models.user.query().update(validUser);
+        req.flash('info', i18next.t('flash.user.create.success'));
         reply.redirect(app.reverse('root'));
       } catch (error) {
+        console.log('could not create new user');
         req.flash('error', i18next.t('flash.users.create.error'));
-        reply.render('users/new', { user: { email }, registrationError: error.message });
+        reply.render('users/new', { user, error });
+      }
+      return reply;
+    })
+    .delete('/users/:id', { preValidation: app.authenticate }, async (req, reply) => {
+      const userId = req.params.id;
+      if (req.user.id !== Number(userId)) {
+        reply.redirect(app.reverse('users'));
+        req.flash('error', i18next.t('flash.users.delete.error'));
+        return reply;
+      }
+      try {
+        await app.objection.models.user.query()
+          .deleteById(userId);
+        /* if (!user) { */
+        req.logOut();
+        req.flash('info', i18next.t('flash.users.delete.success'));
+        reply.redirect(app.reverse('users'));
+        return reply;
+      } catch (error) {
+        console.error(error);
+        req.flash('error', i18next.t('flash.users.delete.error'));
+        return reply.status(500).send('Internal Server Error');
       }
     });
+
+  // eslint-disable-next-line max-len
+  /* TODO: `+buttonTo(route('user', { id: user.id }), 'delete')(class="btn btn-danger" value=t('views.user.delete.submit'))` */
 };
